ba046cc65ebc8700916921c0f17d1e9ec59d98e1
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-12-08 08:19:37 +0300

    fix: sale model

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -1,10 +1,21 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
-    //
+    protected $fillable = [
+        'seller_id','total','sale_date'
+    ];
+
+    public function seller()
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(SaleItem::class);
+    }
 }
