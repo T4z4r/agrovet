b01762022232c3ec63cdcfed8c1d5a4fc87feba6
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-12-09 13:16:59 +0300

    fix

--- database/migrations/2025_12_09_101247_add_category_to_products_table.php
+++ database/migrations/2025_12_09_101247_add_category_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('category');
+            $table->string('category')->nullable();
         });
     }
 
