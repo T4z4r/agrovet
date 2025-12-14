cb86bbcb1d6e777a9d9d3e065f3bcaf88b328c4d
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-12-14 23:23:38 +0300

    fix: login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,6 +57,8 @@
     <script src="{{ asset('assets/vendor/js/template-customizer.js') }}"></script>
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="{{ asset('assets/js/config.js') }}"></script>
+            <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}" />
+
   </head>
 
   <body>
