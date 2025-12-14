31b5de9f5e4ad738a6f7bdd5a3e148ad1c921631
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-12-14 22:35:49 +0300

    fix; minor updates

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -51,9 +51,11 @@
 
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-    <script src="../../assets/vendor/js/template-customizer.js"></script>
+    <script src="{{ asset('assets/vendor/js/template-customizer.js') }}"></script>
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="{{ asset('assets/js/config.js')}}"></script>
+        <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}" />
+
   </head>
 
   <body>
