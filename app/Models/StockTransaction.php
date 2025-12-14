18ca01b244d13f194a8972b8130aa09a8859911b
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-12-14 10:21:29 +0300

    fix: models

--- app/Models/StockTransaction.php
+++ app/Models/StockTransaction.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class StockTransaction extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'type',
+        'quantity',
+        'supplier_id',
+        'recorded_by',
+        'date',
+        'remarks'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'recorded_by');
+    }
 }
