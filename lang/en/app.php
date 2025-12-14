1443b8efa197fc3ff7db2560ba84b72ff0b3106f
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-12-14 23:40:21 +0300

    feat: added language implementation

---
+++ lang/en/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'Dashboard' => 'Dashboard',
+    'Products' => 'Products',
+    'Suppliers' => 'Suppliers',
+    'Sales' => 'Sales',
+    'Expenses' => 'Expenses',
+    'Stock Transactions' => 'Stock Transactions',
+    'Reports' => 'Reports',
+    'Users' => 'Users',
+    'Shops' => 'Shops',
+
+];
---
+++ lang/sw/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'Dashboard' => 'Dashibodi',
+    'Products' => 'Bidhaa',
+    'Suppliers' => 'Wasambazaji',
+    'Sales' => 'Mauzo',
+    'Expenses' => 'Matumizi',
+    'Stock Transactions' => 'Shughuli za Hisa',
+    'Reports' => 'Ripoti',
+    'Users' => 'Watumiaji',
+    'Shops' => 'Maduka',
+
+];
