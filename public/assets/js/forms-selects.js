ee2f5cdec5c7a9b2c2ddb86362240bcb96a33685
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-12-14 17:41:46 +0300

    feat: added web assets

---
+++ public/assets/js/forms-selects.js
@@ -0,0 +1,15 @@
+/**
+ * Selects & Tags
+ */
+
+'use strict';
+
+$(function () {
+  const selectPicker = $('.selectpicker');
+
+  // Bootstrap Select
+  // --------------------------------------------------------------------
+  if (selectPicker.length) {
+    selectPicker.selectpicker();
+  }
+});
