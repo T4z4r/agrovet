0cae0f7fb7e41c895fa1c1cf9266d6a78554c548
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-12-18 02:43:29 +0300

    test again

--- app/Http/Controllers/WebSaleController.php
+++ app/Http/Controllers/WebSaleController.php
@@ -18,7 +18,7 @@ class WebSaleController extends Controller
     public function index(Request $request)
     {
 
-$test = Sale::whereDate('sale_date', Carbon::today())->sum('total');
+$test = Sale::where('sale_date', Carbon::today()->toDateString())->sum('total');
         dd($test);
         if ($request->ajax()) {
             $sales = Sale::with('seller')->latest()->get();
